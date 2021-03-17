bbe2b62990a1bf2ff9aebbc16c7e3c6ceecf2583
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-03-16 20:04:03 -0500

    extension

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Container } from 'react-bootstrap';
 import Post from "./Post";
-import postlist from "../posts"
+// import postlist from "../posts"
 
 export default function App() {
 
-  const excerptList = postlist.map(post => {
-    return post.content.split(" ").slice(0, 20).join(" ") + "..."
-  })
+  // const excerptList = postlist.map(post => {
+  //   return post.content.split(" ").slice(0, 20).join(" ") + "..."
+  // })
 
   return (
     <Container>
