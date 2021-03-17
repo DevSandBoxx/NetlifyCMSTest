a0a1c3d591974bca35e7e28c5b130613321610f2
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-03-16 19:33:13 -0500

    changed extension

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap';
+import Post from "./Post";
+import postlist from "../posts"
 
-function App() {
+export default function App() {
+
+  const excerptList = postlist.map(post => {
+    return post.content.split(" ").slice(0, 20).join(" ") + "..."
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+    </Container>
   );
 }
-
-export default App;
---
+++ src/Post.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container, Row} from "react-bootstrap";
+
+export default function Post(props){
+    return (
+      <Row>
+          <h1>Hello</h1>
+      </Row>
+    );
+}
