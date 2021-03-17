29f9b447de8336501ada49fbe5506fc32e99201b
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-03-17 09:38:23 -0500

    changing data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Container } from 'react-bootstrap';
 
-// import postlist from "../posts"
+// import postlist from "./_posts/blog/2021-03-17-blog-2.json";
+// import * as postlist from "./_posts/blog";
+
 
 export default function App() {
 
