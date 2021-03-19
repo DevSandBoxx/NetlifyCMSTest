ebe809150e9a33209264aaccc6ff3fe8ec065103
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-03-19 09:26:25 -0500

    tests

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import leadershipTeamJSON from "./posts/leadership_team.json";
 
 export default function App() {
 
-
-  console.log("./" + leadershipTeamJSON.positions[0].people[0].photo);
-  const logo = require("./" + leadershipTeamJSON.positions[0].people[0].photo).default;
-
   return (
     <Container>
       <p>{leadershipTeamJSON.positions[0].positions_category}</p>
@@ -15,6 +11,13 @@ export default function App() {
       <p>{leadershipTeamJSON.positions[0].people[0].name}</p>
       <p>{leadershipTeamJSON.positions[0].people[0].title}</p>
       <p>{leadershipTeamJSON.positions[0].people[0].bio}</p>
+      
+      <p>{leadershipTeamJSON.positions[1].positions_category}</p>
+      <img width = "50" alt = "portrait" src = {require("./" + leadershipTeamJSON.positions[0].people[0].photo).default}></img>
+
+      <p>{leadershipTeamJSON.positions[1].people[0].name}</p>
+      <p>{leadershipTeamJSON.positions[1].people[0].title}</p>
+      <p>{leadershipTeamJSON.positions[1].people[0].bio}</p>
     </Container>
   );
 }
